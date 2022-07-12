8e0aa40a5fa42641c749531dae9d30dd928a2c84
Garen <garendemirdogen1@gmail.com>
2022-07-11 21:32:53 -0400

    added variables

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector("currentDay").textContent = moment().format("dddd, MMMM Do YYYY")
+
+var timeFrame = $(".time-frame");
+var list = $("textarea");
+
+for (var i=0; i < timeFrame.length; i++);
