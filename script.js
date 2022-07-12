6fda6b22304fb2826dae75b9653829dda7e6ef67
Garen <garendemirdogen1@gmail.com>
2022-07-11 22:46:05 -0400

    added past, present, future statements

--- script.js
+++ script.js
@@ -6,4 +6,14 @@ window.setInterval(function () {
 var timeBlock = $(".time-block");
 var list = $("textarea");
 
-for (var i=0; i < timeBlock.length; i++);
+for (var i = 0; i < timeBlock.length; i++) {
+    if (setInterval(timeBlock[i].id) < moment().format("h")) {
+        list[i].classList.add("past");
+    }
+    else if (setInterval(timeBlock[i].id) == moment().format("h")) {
+        list[i].classList.add("present");
+    }
+    else if (setInterval(timeBlock[i].id) > moment().format("h")) {
+        list[i].classList.add("future");
+    }
+};
